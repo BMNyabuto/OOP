c083c52b688610df0b00a967c8659ee1ac3afec1
boman <bmnyabuto@gmail.com>
2017-01-19 02:18:51 +0300

    Defined the Property_Management class with object as parameter, declared the class as a metaclass with keyword __metaclass__ and initialised the class variables

--- OOP.py
+++ OOP.py
@@ -1,1 +1,14 @@
 from abc import ABCMeta, abstractmethod
+
+class Property_Management(object):
+	"""Property Renting and To Let Services"""
+	__metaclass__ = ABCMeta
+	
+	Deposit = 0
+	No_of_Rooms = 0
+	
+	def __init__(self, location, Availability_Status):
+		"""Return a new Car Object"""
+		self.location = location            
+		self.Availability_Status = Availability_Status
+		
